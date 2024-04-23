64ffb48fee4f0fabf6eda3ec2695f0b532d7c754
samirkarimov022 <samir.karimov0022@gmail.com>
2024-04-23 06:17:23 +0400

    add

---
+++ Task22/Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using Task22.Json;
+
+namespace Task22
+{
+    internal class Program
+    {
+        
+      // mellim kod bele deyildi error cox idi errorlari sildim qaldi bele belede islemir onsuz :)
+        
+        static void Main(string[] args)
+        {
+
+            List<string> namesList = new List<string>();
+            string json = JsonConvert.SerializeObject(namesList);
+            Add("samir");
+            Add("garib");
+            Add("emil");
+
+            using (StreamReader sr = new StreamReader(@""))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+
+            using (StreamWriter sw = new StreamWriter(@""))
+            {
+                sw.WriteLine(json);
+            }
+            
+            Console.WriteLine( Search("samir")); 
+            Console.WriteLine( Search("garib"));
+        }
+        
+        static void Add(string name) 
+        {
+            Add(name); 
+        }
+        static bool Search(string name)
+        {
+            return Search(name);
+        }
+
+        static void Delete(int index)
+        {
+           
+        }
+        
+    }
+}
